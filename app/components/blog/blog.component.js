e6e6a00c68ccf77c8e0fc7645efd54da53367c18
Oleksii_Podobed <alex.podobed@gmail.com>
2016-12-26 14:09:50 +0200

    refactor: removed `blog-list-pagination` component; moved pagination to `blog` component

--- app/components/blog/blog.component.js
+++ app/components/blog/blog.component.js
@@ -7,27 +7,37 @@ export default {
             'ngInject';
             this.BlogService = BlogService;
             this.$state = $state;
+            this.currentPage = this.$state.params.page;
+            this.itemsPerPage = 5;
+            this.posts = [];
         }
 
         $onInit() {
-            console.info('blog component initialized', this.$state.params.page);
-            this.fetchBlogList(this.$state.params.page);
+            this.loading = true;
+            this.fetchBlogList()
+                .finally(() => this.loading = false)
         }
 
-        fetchBlogList(page) {
-            return this.BlogService.getAll(page)
-                .then((data) => this.blogData = data)
+        fetchBlogList() {
+            return this.BlogService.getAll(this.currentPage)
+                .then((data) => {
+                    this.totalItems = this.itemsPerPage * data.pages;
+                    this.currentPage = data.currentPage;
+                    this.posts = data.posts;
+                })
         }
 
-        fetchAndReload(page){
+        fetchAndReload() {
             this.disabled = true;
-            this.fetchBlogList(page)
-                .then(() => this.reloadState(page))
+            this.fetchBlogList()
+                .then(() => this.reloadState())
                 .finally(() => this.disabled = false)
         }
 
-        reloadState(page) {
-            this.$state.transitionTo(this.$state.current.name, angular.extend(this.$state.params, {page}))
+        reloadState() {
+            this.$state.transitionTo(this.$state.current.name, angular.extend(
+                this.$state.params, {page: this.currentPage})
+            )
         }
     }
 }
--- app/components/blog/blog.module.js
+++ app/components/blog/blog.module.js
@@ -3,7 +3,6 @@ import Pagination from "angular-ui-bootstrap/src/pagination";
 import BlogComponent from './blog.component';
 import BlogDetailsComponent from './blog-details';
 import BlogListComponent from "./blog-list"
-import BlogListPaginationComponent from './blog-list-pagination'
 import {BlogService} from './blog.service';
 
 export default angular
@@ -11,6 +10,5 @@ export default angular
     .component('blog', BlogComponent)
     .component('blogDetails', BlogDetailsComponent)
     .component('blogList', BlogListComponent)
-    .component('blogListPagination', BlogListPaginationComponent)
     .service('BlogService', BlogService)
     .name;
